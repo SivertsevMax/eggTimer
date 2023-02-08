bab1ecc575f512f2198000060acda695a607b388
SivertsevMax <sivertsev1995@icloud.com>
2023-02-08 14:07:20 +0300

    Initial Commit

---
+++ eggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eggTimer
+//
+//  Created by Максим Сиверцев on 08.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
