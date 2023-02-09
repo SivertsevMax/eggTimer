4a2dce45a0270e1afc7961e8ae860087afc152f5
SivertsevMax <sivertsev1995@icloud.com>
2023-02-09 16:26:29 +0300

    добавлен визуал, функция выбора времени

--- eggTimer/ViewController.swift
+++ eggTimer/ViewController.swift
@@ -1,19 +1,46 @@
-//
-//  ViewController.swift
-//  eggTimer
-//
-//  Created by Максим Сиверцев on 08.02.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    let eggTimes = ["soft": 3, "medium": 4, "hard": 7]
+    var startTimer = 0
+    var timer = Timer()
+    
+    @IBOutlet weak var eggTimer: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        eggTimer.text = "CHOOSE YOU LIKE EGGS"
     }
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+        if sender.currentTitle != nil && eggTimes["\(String(sender.currentTitle!))"] != nil {
+            let senderCurrentTittle = sender.currentTitle!
+            switch sender.currentTitle {
+            case "soft":
+                startTimer = eggTimes[senderCurrentTittle]!
+            case "medium":
+                startTimer = eggTimes[senderCurrentTittle]!
+            case "hard":
+                startTimer = eggTimes[senderCurrentTittle]!
+            default: break
+            }
+        } else {
+            eggTimer.text = "error"
+        }
+    }
+    
+    @objc func updateCounter() {
+        if startTimer > 0 && startTimer % 60 < 10 {
+            eggTimer.text = "\(startTimer / 60):0\(startTimer % 60)"
+        } else  if startTimer > 0 && startTimer % 60 >= 10 {
+            eggTimer.text = "\(startTimer / 60):\(startTimer % 60)"
+        } else {
+            eggTimer.text = "YOUR EGG IS COOKED"
+        }
+        startTimer -= 1
+    }
 }
 
